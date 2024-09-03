f6062d692566ecfbe846e23a029fe0de6cb65c3a
Brian Kim <briank1727@gmail.com>
2024-09-03 17:09:18 -0500

    Material UI 9/3/2024

--- material-ui-tut/src/pages/Create.js
+++ material-ui-tut/src/pages/Create.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Typography, Button, Container } from "@material-ui/core";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        className={classes.title}
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        className={classes.btn}
+        onClick={() => console.log("You clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+      {/* <br /> */}
+      {/* <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
+
+      {/* <Button type="submit">SUBMIT</Button>
+      <Button type="submit" color="secondary" variant="outlined">
+        SUBMIT
+      </Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
+  );
 }
